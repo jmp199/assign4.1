3bcdba2a215e7563d0b169610b0ebedc91276d03
jmp199 <65424990+jmp199@users.noreply.github.com>
2020-05-24 21:49:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var number = 2;
+
+
+
+if (number == 2 ) {
+ 
+  number = number + "Number is Even";
+  document.querySelector(".condition").innerHTML = number;
+} else {
+  document.querySelector(".condition").innerHTML = number;
+}
+
+
+var number = 2;
+
+
+
+if (number == 2 ) {
+ 
+  number = number + "Number is positive";
+  document.querySelector(".condition2").innerHTML = number;
+} else {
+  document.querySelector(".condition2").innerHTML = number;
+}
